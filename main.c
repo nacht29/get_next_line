eba31b00aaf17356c2701fc2b1dd92fcd277d20d
Yan Zhe Chan <yachan@u82z04s03.42kl.edu.my>
2024-07-11 17:16:00 +0800

    update

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "get_next_line.h"
+
+int main(int argc, char *argv[])
+{
+    int     fd;
+    char    *line;
+
+    if (argc < 2)
+        return (1);
+    fd = open(argv[1], O_RDONLY);
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s\n", line);
+        free(line);
+    }
+    if (line == NULL)
+        printf("NULL\n");
+}
